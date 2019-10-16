62861ace3da88e666dd351339aeffe36a898e10c
Tetiana Lebid <t.lebed17@gmail.com>
2019-10-16 20:54:35 +0300

    create calendar table and selectors

--- src/app/selector/selector.component.ts
+++ src/app/selector/selector.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DateService } from '../shared/date.service';
 
 @Component({
   selector: 'app-selector',
   templateUrl: './selector.component.html',
   styleUrls: ['./selector.component.scss']
 })
-export class SelectorComponent implements OnInit {
+export class SelectorComponent {
 
-  constructor() { }
+  constructor(private dateService: DateService) { }
 
-  ngOnInit() {
+  go(dir: number) {
+    this.dateService.changeMonth(dir);
   }
-
 }

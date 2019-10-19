276bbb835031a82cf845bc6b4e3c31885fad5774
tetty <t.lebed17@gmail.com>
2019-10-19 23:49:10 +0300

    load all tasks

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DateService } from '../shared/date.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TasksService, Task } from '../shared/tasks.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-organizer',
@@ -11,11 +12,15 @@ import { TasksService, Task } from '../shared/tasks.service';
 export class OrganizerComponent implements OnInit {
 
   form: FormGroup;
+  tasks: Task[] = [];
 
   constructor(private dateService: DateService,
               private tasksService: TasksService) { }
 
   ngOnInit() {
+    this.dateService.date.pipe(
+      switchMap(value => this.tasksService.load(value))
+    ).subscribe(tasks => this.tasks = tasks);
     this.form = new FormGroup({
       title: new FormControl('', Validators.required)
     });
--- src/app/shared/tasks.service.ts
+++ src/app/shared/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 export interface Task {
   id?: string;
@@ -18,6 +19,17 @@ export class TasksService {
   static url = 'https://organizer-c782d.firebaseio.com/tasks';
 
   constructor(private http: HttpClient) {
+  }
+
+  load(date: moment.Moment): Observable<Task[]> {
+    return this.http
+      .get<Task[]>(`${TasksService.url}/${date.format('DD-MM-YYYY')}.json`)
+      .pipe(map(tasks => {
+        if (!tasks) {
+          return [];
+        }
+        return Object.keys(tasks).map(key => ({...tasks[key], id: key}));
+    }));
   }
 
   create(task: Task): Observable<Task> {
